f6a16c99e5b532a9a61687f94311222ac233157b
javaSwing <zxdlovejava@gmail.com>
2020-08-02 09:36:59 +0800

    feat: 更新http方法options

---
+++ http/code/options-methods/vue-code/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <button @click="handleClick">发送请求</button>
+  </div>
+</template>
+
+<script>
+import { getList } from '@/api'
+export default {
+  name: 'App',
+  components: {
+  },
+  methods: {
+    async handleClick () {
+      getList()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
